d4dd091d73b7e270381bbcf196f1ceb8feb24632
Kenneth Keller <kenkanifffromct21@gmail.com>
2021-03-31 19:16:38 -0400

    updated db name

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Workout',
+  process.env.MONGODB_URI || 'mongodb://localhost/workouts',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
